e6235493ee2dd59ee981525ed6aca4b13795b480
Th0rn0 <Th0rn0@lanops.co.uk>
2019-08-26 15:46:25 +0100

    GPIO LED & Readme update

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Py-UsbRipper
 
-WIP
+**WIP**
+
+Python3 Script to run on a Raspberry pi to rip USB storage devices and SD Cards automatically and save them to a backup directory ready for transfer onto a server (via WiFi - WIP). Currently has GPIOZero for LED usage (pins: red=9, green=10, blue=11).
+
+## Prerequisites
+
+- Python3
+- pyudev
+- os
+- psutil
+- time
+- distutils.dir_util
+- gpiozero
+
+## Usage
+
+Run the script and insert a USB, wait.
--- usbripper.py
+++ usbripper.py
@@ -8,19 +8,31 @@ import psutil
 import time
 
 from distutils.dir_util import copy_tree
+from gpiozero import RGBLED
 
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('block')
 toDirectory = "/backups"
+# LEDs
+led = RGBLED(red=9, green=10, blue=11)
+# Make all LEDs red while boot up
+led.red = 1
 for device in iter(monitor.poll, None):
+    # Device is now ready to poll
+    led.color = (0, 1, 0)  # full green
     if device.action == 'add':
         print('{} connected'.format(device))
         print('{} PATH'.format(device.device_node))
         for p in psutil.disk_partitions():
             print(p.mountpoint)
             if p.device == device.device_node:
-                print("  {}: {}".format(p.device, p.mountpoint))
+                # DEBUG
+                # print("  {}: {}".format(p.device, p.mountpoint))
+                # We've found a mount point. Begin copy
+                led.color = (1, 1, 0)  # full yellow
+                print('Copying {} to {}'.format(p.mountpoint, toDirectory))
                 copy_tree(p.mountpoint, toDirectory, verbose=1)
                 print('done')
+                led.color = (0, 1, 0)  # full green
         print('----------');
