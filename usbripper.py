868062286a0482454f27bf7549fa62057d8e9d78
Th0rn0 <Th0rn0@lanops.co.uk>
2019-09-05 18:46:19 +0100

    block for backup device

--- usbripper.py
+++ usbripper.py
@@ -14,6 +14,7 @@ context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('block')
 toDirectory = "/backups"
+backupDriveUuid = "2d42aa7b-a00b-42c7-8c73-90241155e8ed"
 # LEDs
 led = RGBLED(red=9, green=10, blue=11)
 # Make all LEDs red while boot up
@@ -24,15 +25,17 @@ for device in iter(monitor.poll, None):
     if device.action == 'add':
         print('{} connected'.format(device))
         print('{} PATH'.format(device.device_node))
+        print('{} UUID'.format(device.get('ID_FS_UUID')))
         for p in psutil.disk_partitions():
             print(p.mountpoint)
-            if p.device == device.device_node:
-                # DEBUG
-                # print("  {}: {}".format(p.device, p.mountpoint))
-                # We've found a mount point. Begin copy
-                led.color = (1, 1, 0)  # full yellow
-                print('Copying {} to {}'.format(p.mountpoint, toDirectory))
-                copy_tree(p.mountpoint, toDirectory, verbose=1)
-                print('done')
-                led.color = (0, 1, 0)  # full green
+            if device.get('ID_FS_UUID') != backupDriveUuid:
+                if p.device == device.device_node:
+                    # DEBUG
+                    # print("  {}: {}".format(p.device, p.mountpoint))
+                    # We've found a mount point. Begin copy
+                    led.color = (1, 1, 0)  # full yellow
+                    print('Copying {} to {}'.format(p.mountpoint, toDirectory))
+                    copy_tree(p.mountpoint, toDirectory, verbose=1)
+                    print('done')
+                    led.color = (0, 1, 0)  # full green
         print('----------');
